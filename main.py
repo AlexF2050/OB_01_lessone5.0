22d954a5980c2c870ba5904cb54c3c03c4ce1a0f
AlexF2050 <findex2050@gmail.com>
2024-10-18 10:50:01 +0700

    add new code

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 class Warrior(): # Класс персонажа
     def __init__(self, name, power, endurance, hair_color): #конструктор класса Warrior с параметрами name, power, hair_color
-        self.name = name # имя персонажа
-        self.power = power # уровень силы персонажа
-        self.endurance = endurance # уровень силы персонажа
-        self.hair_color = hair_color # цвет волос
-
+        self.name = name # имя персонажа = характеристика
+        self.power = power # уровень энергии силы' персонажа = характеристика
+        self.endurance = endurance # уровень выносливости' персонажа= характеристика
+        self.hair_color = hair_color # цвет волос = характеристика
+#Методы класса Warrior
     def sleep(self):
         print(f"{self.name} лёг отдохнуть") # выводим сообщение
         self.endurance += 2# увеличиваем уровень силы персонажа на 2
@@ -25,3 +25,33 @@ class Warrior(): # Класс персонажа
         print(f"цвет волос воина - {self.hair_color}") # выводим сообщение
         print(f"сила воина  - {self.power}") # выводим сообщение
         print(f"выносливость воина  - {self.endurance}")  # выводим сообщение
+# Создаём Объект для класса Warrior_____________________________________________________________________________________
+war1 = Warrior("ZeroBot", 88, 77, "черно-серебристый") # создаём объект класса Warrior
+war2 = Warrior("NeroBaz", 55, 95, "белый") # создаём объект класса Warrior
+
+print(war2.name) # выводим сообщение
+print(war2.power) # выводим сообщение
+print(war2.endurance) # выводим сообщение
+print(war2.hair_color) # выводим сообщение
+print("       ")
+
+print(war2.endurance) # выводим сообщение
+print(war2.power) # выводим сообщение
+war2.sleep()  # выводим сообщение
+war2.eat()  # выводим сообщение
+print(war2.endurance) # выводим сообщение
+print(war2.power)
+print("       ")
+
+war1.sleep()  # выводим сообщение
+war1.eat()  # выводим сообщение
+war1.nit()  # выводим сообщение
+war1.walk()  # выводим сообщение
+war1.info()  # выводим сообщение
+print("       ")
+
+war2.sleep()  # выводим сообщение
+war2.eat()  # выводим сообщение
+war2.nit()  # выводим сообщение
+war2.walk()  # выводим сообщение
+war2.info()  # выводим сообщение
